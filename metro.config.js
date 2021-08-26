8ba0caccff01f060eef06aa975bc55aad0ad0b78
Stabien <bastien.piedallu@gmail.com>
2021-08-26 21:47:55 +0200

    debugging

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,9 @@
-const { getDefaultConfig } = require('@expo/metro-config');
+const { getDefaultConfig } = require('expo/metro-config');
 
 const defaultConfig = getDefaultConfig(__dirname);
 
-defaultConfig.resolver.assetExts.push('db');
-
-module.exports = defaultConfig;
+module.exports = {
+  resolver: {
+    assetExts: [...defaultConfig.resolver.assetExts, 'db'],
+  },
+};
