a1108d87364543ab3967af445cc877b97da6bc85
Stabien <bastien.piedallu@gmail.com>
2021-08-29 13:38:46 +0200

    debugging

--- data/controllers.js
+++ data/controllers.js
@@ -20,13 +20,27 @@ export const getItemsFromResearch = async (research) => {
   const db = await openDatabase();
   const optimizedResearch = research.replace(/\s/g, '%');
   const accentRegex = /[\u0300-\u036f]/g;
-  let request =
+  let request;
+
+  if (accentRegex.test(optimizedResearch.normalize("NFD")))
+    request =
       "SELECT alim_nom_fr AS name, alim_code AS id, alim_grp_nom_fr AS categoryName, " +
       "alim_ssgrp_nom_fr AS typeName, alim_ssgrp_code AS typeId " +
       "FROM aliments " +
       "WHERE alim_nom_fr LIKE '" + optimizedResearch + "%' " +
       "OR alim_nom_fr LIKE '% " + optimizedResearch + "%' " +
       "GROUP BY alim_code, alim_nom_fr, alim_grp_nom_fr, alim_ssgrp_nom_fr, alim_ssgrp_code " +
+      "ORDER BY CASE WHEN alim_nom_fr LIKE '" + optimizedResearch + "%' " +
+      "then 0 else 1 end, Length(alim_nom_fr), alim_nom_fr"
+    ;
+  else
+    request =
+      "SELECT alim_nom_fr AS name, alim_code AS id, alim_grp_nom_fr AS categoryName, " +
+      "alim_ssgrp_nom_fr AS typeName, alim_ssgrp_code AS typeId, nom_sans_accents " +
+      "FROM aliments " +
+      "WHERE nom_sans_accents LIKE '" + optimizedResearch + "%' " +
+      "OR nom_sans_accents LIKE '% " + optimizedResearch + "%' " +
+      "GROUP BY alim_code, alim_nom_fr, alim_grp_nom_fr, alim_ssgrp_nom_fr, alim_ssgrp_code, nom_sans_accents " +
       "ORDER BY CASE WHEN alim_nom_fr LIKE '" + optimizedResearch + "%' " +
       "then 0 else 1 end, Length(alim_nom_fr), alim_nom_fr"
     ;
